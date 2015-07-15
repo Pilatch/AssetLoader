8053f095fad649245ce92f7aa6c74af5fa25a28b
Ethan B Martin <ethan@pilatch.com>
2015-07-15 13:23:20 -0400

    Main page added to help point to documentation and tests. Added .eslintrc to conform to style guide in the future.

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-var express = require('express')
-var jadeStatic = require('jade-static')
-var args = require('yargs').argv
+var express = require("express")
+var jadeStatic = require("jade-static")
+var args = require("yargs").argv
 var app = express()
 var port = args.port || 3001
 
 app.use( jadeStatic(__dirname) )
 app.use( express.static(__dirname) )
 app.listen(port)
+console.log("server should be running at http://localhost:" + port)
